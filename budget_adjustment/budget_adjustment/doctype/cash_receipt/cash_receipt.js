7c140f215fe8a760d30a84db0ffec3dc770f8cc0
njoguchris <chris@pointershub.com>
2021-09-16 03:33:17 +0000

    cash register & purchase request

--- budget_adjustment/budget_adjustment/doctype/cash_receipt/cash_receipt.js
+++ budget_adjustment/budget_adjustment/doctype/cash_receipt/cash_receipt.js
@@ -1,6 +1,5 @@
 // Copyright (c) 2021, Chris and contributors
 // For license information, please see license.txt
-let totals = 0;
 frappe.ui.form.on('Cash Receipt', {
 	refresh: function(frm) {
 		if(frm.doc.docstatus == 1){
@@ -10,6 +9,8 @@ frappe.ui.form.on('Cash Receipt', {
 
 			cur_frm.page.set_inner_btn_group_as_primary(__('Create'));
 		}
+
+
 
 	},
     	onload: function(frm) {
@@ -33,6 +34,15 @@ frappe.ui.form.on('Cash Receipt', {
 			    };
 		    });
 
+            frm.set_query("budget", function() {
+			    return {
+				    filters: {
+					    company: frm.doc.company,
+						cost_center: frm.doc.cost_center
+				    }
+			    };
+		    });
+
 
 			frm.set_query("received_account", "cash_receipts", function() {
 				return {
@@ -44,17 +54,56 @@ frappe.ui.form.on('Cash Receipt', {
                 }
 			});
         },
+		budget:function(frm){
+
+					frm.refresh_field('cash_receipts');
+					frm.clear_table("cash_receipts");
+
+			if(frm.doc.budget) {
+				frappe.call({
+					type: "GET",
+					method: "budget_adjustment.api.get_budget_child_accounts",
+					args: {"budget": frm.doc.budget},
+					callback: function (r) {
+						if (r.message.docstatus != 1) {
+							frappe.throw("The Budget Is Cancelled Budget cannot be loaded");
+							frm.clear_table("cash_receipts");
+							frm.refresh_field('cash_receipts');
+							frm.refresh_field('budget');
+						} else {
+
+							let b_accounts = r.message.accounts;
+							b_accounts.forEach(b_account => {
+								frm.add_child('cash_receipts', {
+									received_account: b_account.account,
+									previous_budget_amount: b_account.budget_amount
+
+								});
+							});
+							frm.refresh_field('cash_receipts');
 
 
+						}
+					}
+				});
+			}else{
+					frm.clear_table("cash_receipts");
+					frm.refresh_field('cash_receipts');
+			}
 
-});
-frappe.ui.form.on('Cash Receipt Item', {
+		},
+	before_save: function(frm){
+		var total = 0;
+		$.each(frm.doc.cash_receipts || [], function(i, d) {
+			total += flt(d.amount_received);
+		});
+		frm.set_value("total", total);
 
-	amount_received: function(frm,cdt,cdn){
-		var items = locals[cdt][cdn];
-		totals += items.amount_received;
-		frm.set_value("total", totals);
-
-
+		if(frm.doc.total !== frm.doc.received_amount){
+			frappe.throw("The Received Amount and the total allocated amount should be equal");
+		}
 	}
+
+
+
 });
